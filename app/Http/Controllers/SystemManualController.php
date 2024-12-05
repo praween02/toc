362e33e3483565aa8487767a2ef51a6960662d15
Vivek Gupta <108297355+Vivek-0512@users.noreply.github.com>
2024-12-05 16:23:18 +0530

    fix issue form submit

--- app/Http/Controllers/SystemManualController.php
+++ app/Http/Controllers/SystemManualController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\DataTables\SystemManualDataTable;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Contracts\Encryption\DecryptException;
+use Session;
 //Load Model
 use App\Models\{Institute, ProjectTimeline, SystemManual};
 
@@ -46,13 +47,13 @@ class SystemManualController extends Controller
     public function store(Request $request)
     {
         $validatedData = $request->validate([
-            'equipment_id' => 'required|exists:equipments,id',
             'document_title' => 'required|string|max:255',
             'document_description' => 'nullable|string',
             'document_file' => 'required|file|mimes:pdf|max:10240', // 10MB max for the PDF
             'no_of_page' => 'required|integer',
             'type' => 'required|integer',
         ]);
+        $requestArr = $request->all();
 
         // Handle the file upload
         if ($request->hasFile('document_file')) {
@@ -62,14 +63,14 @@ class SystemManualController extends Controller
 
         // Create the new record in the Tikal table
         $tikal = SystemManual::create([
-            'equipment_id' => $validatedData['equipment_id'],
+            'equipment_id' => $requestArr['equipment_id'] ?? 0,
             'document_title' => $validatedData['document_title'],
             'document_description' => $validatedData['document_description'],
             'document_file' => $documentFilePath ?? null,  // Save file path if file exists
             'no_of_page' => $validatedData['no_of_page'],
             'type' => $validatedData['type'],
         ]);
-
+        Session::flash('message', 'Submitted successfully'); 
         // Redirect back or to a new page with success message
         return redirect()->route('system_manual.index')->with('success', 'Data saved successfully!');
 
